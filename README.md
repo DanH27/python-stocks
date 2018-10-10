1130f0cd6d168876cec8811c59909b57cc942daf
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2018-10-10 19:28:18 -0400

    Complete project

---
+++ README.md
@@ -0,0 +1,35 @@
+# Python Stock App
+
+## Discovery / Requirements
+### What data do we need? ###
+The data that we need to use is the one from https://www.alphavantage.co/. I used the data by month and chose to do the end of each year from the past five years.
+
+### Who needs it ###
+I needed this data to use for this lab to compare different stock prices (highs in this case).
+
+### Why is it important? ###
+It is important because if someone is going to invest in the stock market they need to know what stock to invest in.
+
+### What are our goals? ###
+My goal is to compare different stocks and see which one is the most likely to increase in value.
+
+### What is the business value ###
+The business value is the return value of buying a stock.
+
+## Collection Phase ##
+I used the request module for python to get data about stock prices.
+
+## Data Prep / Cleaning ##
+The data just needed to be filtered by year.
+
+## Exploration/Planning ##
+There was no missing data in my excel file. The only problem was too much data.
+
+## Modeling/Algorithms ##
+I used python's matplotlib to sort through the stocks.
+
+## Automation / Computation ##
+I used functions from matplotlib to add the points for each stock (quarterly, only one per year), to create the bar chart. This was very useful because the functions were very descriptive and it was very easy to use.
+
+## Findings / Review / Repeat ##
+After all the stock data was analyzed I decided the best option to invest in was Tesla. They are having some problems right now with the publicity but I think it will eventually all blow over and the stocks will quickly rebound. The highs it reached this year where by far the highest. And with all the new products and innovation coming out of Tesla I feel like this is a safe bet to invest in as Tesla shows the have tons of potential.
---
+++ app.py
@@ -0,0 +1,117 @@
+#from flask import Flask
+import requests
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+#app = Flask(__name__)
+teslaHighs = []
+microsoftHighs = []
+appleHighs = []
+facebookHighs = []
+zyngaHighs = []
+
+
+#Tesla Section
+tesla = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=TSLA&outputsize=full&apikey=JQ5OZI602IGDD72C")
+
+teslaHighs.append(tesla.json()["Monthly Time Series"]["2018-10-10"]["2. high"])
+teslaHighs.append(tesla.json()["Monthly Time Series"]["2017-12-29"]["2. high"])
+teslaHighs.append(tesla.json()["Monthly Time Series"]["2016-12-30"]["2. high"])
+teslaHighs.append(tesla.json()["Monthly Time Series"]["2015-12-31"]["2. high"])
+teslaHighs.append(tesla.json()["Monthly Time Series"]["2014-12-31"]["2. high"])
+
+#Microsoft Section
+microsoft = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&outputsize=full&apikey=JQ5OZI602IGDD72C")
+microsoftHighs.append(microsoft.json()["Monthly Time Series"]["2018-10-10"]["2. high"])
+microsoftHighs.append(microsoft.json()["Monthly Time Series"]["2017-12-29"]["2. high"])
+microsoftHighs.append(microsoft.json()["Monthly Time Series"]["2016-12-30"]["2. high"])
+microsoftHighs.append(microsoft.json()["Monthly Time Series"]["2015-12-31"]["2. high"])
+microsoftHighs.append(microsoft.json()["Monthly Time Series"]["2014-12-31"]["2. high"])
+
+#Apple
+apple = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=AAPL&outputsize=full&apikey=JQ5OZI602IGDD72C")
+appleHighs.append(apple.json()["Monthly Time Series"]["2018-10-10"]["2. high"])
+appleHighs.append(apple.json()["Monthly Time Series"]["2017-12-29"]["2. high"])
+appleHighs.append(apple.json()["Monthly Time Series"]["2016-12-30"]["2. high"])
+appleHighs.append(apple.json()["Monthly Time Series"]["2015-12-31"]["2. high"])
+appleHighs.append(apple.json()["Monthly Time Series"]["2014-12-31"]["2. high"])
+
+#Facebook
+facebook = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=FB&outputsize=full&apikey=JQ5OZI602IGDD72C")
+facebookHighs.append(facebook.json()["Monthly Time Series"]["2018-10-10"]["2. high"])
+facebookHighs.append(facebook.json()["Monthly Time Series"]["2017-12-29"]["2. high"])
+facebookHighs.append(facebook.json()["Monthly Time Series"]["2016-12-30"]["2. high"])
+facebookHighs.append(facebook.json()["Monthly Time Series"]["2015-12-31"]["2. high"])
+facebookHighs.append(facebook.json()["Monthly Time Series"]["2014-12-31"]["2. high"])
+
+#Zynga
+zynga = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=ZNGA&outputsize=full&apikey=JQ5OZI602IGDD72C")
+zyngaHighs.append(zynga.json()["Monthly Time Series"]["2018-10-10"]["2. high"])
+zyngaHighs.append(zynga.json()["Monthly Time Series"]["2017-12-29"]["2. high"])
+zyngaHighs.append(zynga.json()["Monthly Time Series"]["2016-12-30"]["2. high"])
+zyngaHighs.append(zynga.json()["Monthly Time Series"]["2015-12-31"]["2. high"])
+zyngaHighs.append(zynga.json()["Monthly Time Series"]["2014-12-31"]["2. high"])
+
+# names = ["2018-10-10", "2017-12-29", "2016-12-30", "2015-12-31", "2014-12-31"]
+# values = teslaHighs
+# values.sort()
+
+
+# plt.subplot(131)
+# plt.bar(names, values)
+# plt.xticks(rotation=90)
+# plt.xlabel('Years')
+# plt.ylabel('Stock Prices (High)')
+# plt.show()
+#
+# names = ["2018-10-10", "2017-12-29", "2016-12-30", "2015-12-31", "2014-12-31"]
+# values = microsoftHighs
+# values.sort()
+#
+#
+# plt.subplot(131)
+# plt.bar(names, values)
+# plt.xticks(rotation=90)
+# plt.xlabel('Years')
+# plt.ylabel('Stock Prices (High)')
+# plt.show()
+
+
+# names = ["2018-10-10", "2017-12-29", "2016-12-30", "2015-12-31", "2014-12-31"]
+# values = facebookHighs
+# values.sort()
+#
+#
+# plt.subplot(131)
+# plt.bar(names, values)
+# plt.xticks(rotation=90)
+# plt.xlabel('Years')
+# plt.ylabel('Stock Prices (High)')
+# plt.show()
+
+
+# names = ["2018-10-10", "2017-12-29", "2016-12-30", "2015-12-31", "2014-12-31"]
+# values = zyngaHighs
+# values.sort()
+#
+#
+# plt.subplot(131)
+# plt.bar(names, values)
+# plt.xticks(rotation=90)
+# plt.xlabel('Years')
+# plt.ylabel('Stock Prices (High)')
+# plt.show()
+#
+#
+names = ["2018-10-10", "2017-12-29", "2016-12-30", "2015-12-31", "2014-12-31"]
+values = appleHighs
+values.sort()
+
+
+plt.subplot(131)
+plt.bar(names, values)
+plt.xticks(rotation=90)
+plt.xlabel('Years')
+plt.ylabel('Stock Prices (High)')
+plt.show()
